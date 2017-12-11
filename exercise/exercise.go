0e7e4612e990c27cea96d9c57fdca5901982ad2d
Ariel Alonso <xsaint32@gmail.com>
2017-12-11 09:39:23 -0500

    Remove instructions

--- exercise/exercise.go
+++ exercise/exercise.go
@@ -15,43 +15,43 @@ import (
 	"github.com/dotariel/advent-of-go/library/tower"
 )
 
-var exercises = make(map[int]Exercise)
+var days = make(map[int]Exercise)
 
 type Part func(string) interface{}
 type Exercise []Part
 
 func init() {
-	exercises[1] = Exercise([]Part{
+	days[1] = Exercise([]Part{
 		func(input string) interface{} { return captcha.InverseCaptcha(input, captcha.WrapAroundAdd) },
 		func(input string) interface{} { return captcha.InverseCaptcha(input, captcha.HalfwayAroundAdd) },
 	})
-	exercises[2] = Exercise([]Part{
+	days[2] = Exercise([]Part{
 		func(input string) interface{} { return checksum.NewSpreadsheet(input).Checksum(checksum.Diff) },
 		func(input string) interface{} { return checksum.NewSpreadsheet(input).Checksum(checksum.Factor) },
 	})
-	exercises[3] = Exercise([]Part{
+	days[3] = Exercise([]Part{
 		func(input string) interface{} { return "NOT IMPLEMENTED" },
 		func(input string) interface{} { return "NOT IMPLEMENTED" },
 	})
-	exercises[4] = Exercise([]Part{
+	days[4] = Exercise([]Part{
 		func(input string) interface{} { return passphrase.CountValid(input, passphrase.Unique) },
 		func(input string) interface{} { return passphrase.CountValid(input, passphrase.NonAnagram) },
 	})
-	exercises[5] = Exercise([]Part{
+	days[5] = Exercise([]Part{
 		func(input string) interface{} { return stack.NewStack(input).Trace(stack.SimpleIncrementer) },
 		func(input string) interface{} { return stack.NewStack(input).Trace(stack.BiasedDecrementer) },
 	})
-	exercises[6] = Exercise([]Part{
+	days[6] = Exercise([]Part{
 		func(input string) interface{} { return memory.NewState(input).CountCycles() },
 		// func(input string) interface{} { return stack.NewStack(input).Trace(stack.BiasedDecrementer) },
 	})
-	exercises[7] = Exercise([]Part{
+	days[7] = Exercise([]Part{
 		func(input string) interface{} { return tower.New(input).Name },
 		func(input string) interface{} {
 			return tower.FindMismatch(tower.New(input))
 		},
 	})
-	exercises[8] = Exercise([]Part{
+	days[8] = Exercise([]Part{
 		func(input string) interface{} {
 			registers := register.New()
 			registers.ProcessBatch(input)
@@ -63,7 +63,7 @@ func init() {
 			return registers.ProcessBatch(input)
 		},
 	})
-	exercises[9] = Exercise([]Part{
+	days[9] = Exercise([]Part{
 		func(input string) interface{} {
 			score := 0
 			groups, _, _ := stream.Parse(input)
@@ -81,17 +81,19 @@ func init() {
 
 }
 
-func Run(day int) error {
-	file := fmt.Sprintf("%v/src/github.com/dotariel/advent-of-go/exercise/inputs/%d", os.Getenv("GOPATH"), day)
+func Run(d int) error {
+	file := fmt.Sprintf("%v/src/github.com/dotariel/advent-of-go/exercise/inputs/%d", os.Getenv("GOPATH"), d)
 	bytes, _ := ioutil.ReadFile(file)
 
-	exercise, ok := exercises[day]
+	day, ok := days[d]
 
 	if !ok {
-		return fmt.Errorf("no exercise found for day %v", day)
+		return fmt.Errorf("no exercise found for day %v", d)
 	}
 
-	for part, fn := range exercise {
+	fmt.Printf("Day %d: https://adventofcode.com/2017/day/%v\n", d, d)
+
+	for part, fn := range day {
 		fmt.Printf("Part %v: %v\n", part+1, fn(string(bytes)))
 	}
 
